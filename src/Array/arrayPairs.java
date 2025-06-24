95602f46ddb4f7f70f35c1c0c14764f0e0a4872d
Ravishankar Mahto <ravishankarkumar326@yahoo.com>
2025-06-24 17:39:25 +0000

    first commit

---
+++ src/Array/arrayPairs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class arrayPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println("Pairs of elements in the array:");
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+        
+        sc.close();
+    }
+}
+// This program takes an array of integers as input and prints all unique pairs of elements in the array.
